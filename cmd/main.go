c6bf58d0ae4aa2d2ee70134d15bbb3f749df619f
viniciusfranca019 <viniciusfrancas019@gmail.com>
2024-05-15 09:17:21 -0300

    add skeleton for home page

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,29 @@
 package main
 
-import "fmt"
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+type PageData struct {
+	Title   string
+	Content string
+	Items   []string
+}
 
 func main() {
-	fmt.Println("hello world")
+	router := gin.Default()
+
+	router.LoadHTMLGlob("./app/Views/Templates/**/*.html")
+
+	router.GET("/", func(c *gin.Context) {
+		data := PageData{
+			Title:   "Example Page",
+			Content: "Welcome to the example page!",
+			Items:   []string{"Item 1", "Item 2", "Item 3"},
+		}
+		c.HTML(http.StatusOK, "List.html", gin.H{"data": data})
+	})
+
+	router.Run(":8080")
 }
